32e98dabc880fd64dc5d9722670fb5b2e91f43fd
Reshi <duuhmorciani@gmail.com>
2025-05-10 21:01:39 -0300

    Adiciona a classe OpcoesMenu

---
+++ src/OpcoesMenu.java
@@ -0,0 +1,18 @@
+public class OpcoesMenu {
+
+    public static final String MENU = "\n|--------MENU--------|";
+
+    public static final String IMPRIMIR_ARVORE = "Imprimir Árvore";
+    public static final String ARVORE_CODIGO_MORSE = "Árvore dos Códigos Morse";
+    public static final String ARVORE_LETRAS = "Árvore das Letras";
+
+    public static final String TABELA_LETRA_CODIGO = "Mostrar tabela de conversão";
+    public static final String MENSAGEM = "Inserir mensagem";
+
+    public static final String MENSAGEM_COMUM = "Mensagem em código morse";
+    public static final String MENSAGEM_MORSE = "Mensagem em código morse";
+
+    public static final String VOLTAR = "Voltar";
+    public static final String SAIR = "Sair";
+
+}
