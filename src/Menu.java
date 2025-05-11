e3f7aee7868e9bb316e39df8fafc29b55746dc94
Reshi <duuhmorciani@gmail.com>
2025-05-10 22:36:51 -0300

    Adiciona a classe Menu

---
+++ src/Menu.java
@@ -0,0 +1,38 @@
+public class Menu {
+    public void menuPrincipal() {
+        System.out.println(OpcoesMenu.MENU);
+        String[] opcoes = {OpcoesMenu.IMPRIMIR_ARVORE, OpcoesMenu.TABELA_LETRA_CODIGO, OpcoesMenu.MENSAGEM, OpcoesMenu.SAIR};
+        exibirMenu(opcoes);
+    }
+
+    public void menuArvore() {
+        System.out.println(OpcoesMenu.IMPRIMIR_ARVORE);
+        String[] opcoes = {OpcoesMenu.ARVORE_CODIGO_MORSE, OpcoesMenu.ARVORE_LETRAS,OpcoesMenu.VOLTAR};
+        exibirMenu(opcoes);
+    }
+
+    public void mostrarArvoreMorse(){
+        System.out.println(OpcoesMenu.ARVORE_CODIGO_MORSE);
+    }
+
+    public void mostrarArvoreLetras(){
+        System.out.println(OpcoesMenu.ARVORE_LETRAS);
+    }
+
+    public void mostratabela() {
+        System.out.println(OpcoesMenu.TABELA_LETRA_CODIGO);
+    }
+
+    public void menuMensagem() {
+        System.out.println(OpcoesMenu.MENSAGEM);
+        String[] opcoes = {OpcoesMenu.MENSAGEM_COMUM, OpcoesMenu.MENSAGEM_MORSE,OpcoesMenu.VOLTAR};
+        exibirMenu(opcoes);
+    }
+
+
+    public void exibirMenu(String [] opcoes){
+        for( int i = 0 ; i < opcoes.length; i ++){
+            System.out.printf("[ %d ] %s%n", i + 1, opcoes[i]);
+        }
+    }
+}
