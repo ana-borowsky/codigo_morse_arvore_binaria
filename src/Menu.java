81dfb75d6669ab3f9d78b46d4e0320f95ee8ccdb
Giovanni Strasser <53452941+romm27@users.noreply.github.com>
2025-05-11 11:39:12 -0300

    Embeleza area de input

--- src/Menu.java
+++ src/Menu.java
@@ -3,12 +3,15 @@ public class Menu {
         System.out.println(OpcoesMenu.MENU);
         String[] opcoes = {OpcoesMenu.IMPRIMIR_ARVORE, OpcoesMenu.TABELA_LETRA_CODIGO, OpcoesMenu.MENSAGEM, OpcoesMenu.SAIR};
         exibirMenu(opcoes);
+        imprimirSetaInput();
+        
     }
 
     public void menuArvore() {
         System.out.println(Mensagens.DESTAQUE + OpcoesMenu.IMPRIMIR_ARVORE);
         String[] opcoes = {OpcoesMenu.ARVORE_CODIGO_MORSE, OpcoesMenu.ARVORE_LETRAS,OpcoesMenu.VOLTAR};
         exibirMenu(opcoes);
+        imprimirSetaInput();
     }
 
     public void mostrarArvoreMorse() {
@@ -27,11 +30,20 @@ public class Menu {
         System.out.println(Mensagens.DESTAQUE + OpcoesMenu.MENSAGEM);
         String[] opcoes = {OpcoesMenu.MENSAGEM_COMUM, OpcoesMenu.MENSAGEM_MORSE,OpcoesMenu.VOLTAR};
         exibirMenu(opcoes);
+        imprimirSetaInput();
     }
 
     public void exibirMenu(String [] opcoes) {
         for( int i = 0 ; i < opcoes.length; i ++) {
             System.out.printf("[ %d ] %s%n", i + 1, opcoes[i]);
         }
+    }
+    
+    private void imprimirSetaInput() {
+    	System.out.print(">> ");
+    }
+    
+    private void segurarParaContinuar() {
+    	
     }
 }
